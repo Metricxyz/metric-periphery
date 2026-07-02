409c7bfb046aa91a08f5b046e8ed490c065b0964
Nradko <konrad.wierzbik@gmail.com>
2026-07-02 10:49:38 +0200

    Fix onlyPool modifier in BaseMetricExtension

--- test/AllowlistFactoryStub.sol
+++ test/AllowlistFactoryStub.sol
@@ -1,12 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.35;
 
-/// @dev Minimal factory stub: periphery allowlist contracts only call `poolAdmin(pool)`.
+/// @dev Minimal factory stub for extension tests: pool registry and admin lookups.
 contract AllowlistFactoryStub {
   mapping(address => address) internal _poolAdmin;
+  mapping(address => bool) public isPool;
 
   function setPoolAdmin(address pool, address admin_) external {
     _poolAdmin[pool] = admin_;
+    isPool[pool] = true;
   }
 
   function poolAdmin(address pool) external view returns (address) {
